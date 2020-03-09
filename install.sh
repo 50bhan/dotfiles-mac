c957b928b6e9475c922b29e890725356ece9ab02
50bhan <sobhan.sharifi.sadeghi@gmail.com>
2020-03-09 22:21:45 +0330

    Update installer

--- install.sh
+++ install.sh
@@ -3,7 +3,66 @@
 # Hide "last login" line when starting a new terminal session
 touch $HOME/.hushlogin
 
-# Install zsh
+# ------------------------------------ Update & Upgrade ---------------------------------
+apt clean && apt update -y && apt upgrade -y &&
+# ------------------------------------ Install Tools ------------------------------------
+apt install -yq --fix-missing \
+    net-tools \
+    network-manager-openvpn-gnome network-manager-pptp network-manager-pptp-gnome network-manager-vpnc network-manager-vpnc-gnome \
+    network-manager-openconnect network-manager-openconnect-gnome \
+    build-essential \
+    software-properties-common \
+    unrar \
+    gcc \
+    make \
+    curl \
+    tilix \
+    htop \
+    vim \
+    tcptrack \
+    lnav \
+    geoip-bin \
+    mysql-server &&
+# ------------------------------------ Install & config DNS ------------------------------------
+apt install -y resolvconf &&
+systemctl stop systemd-resolved && # Stop systemd-resolved to avoid overwrite resolv.conf file
+systemctl disable systemd-resolved && # Disable systemd-resolved
+echo nameserver 8.8.8.8 > /etc/resolvconf/resolv.conf.d/tail && # Set "Google" DNS addresses
+echo nameserver 4.2.2.4 >> /etc/resolvconf/resolv.conf.d/tail && # Set "Google" DNS addresses
+resolvconf -u # Make DNS addresses permanent
+# ------------------------------------ Install Docker CE ---------------------------------------
+# Install packages to allow apt to use a repository over HTTPS
+apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common &&
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && 
+
+# Add Docker stable repository
+add-apt-repository -y \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" &&
+   
+apt update -y &&
+apt install -y docker-ce docker-ce-cli containerd.io &&
+
+# Run docker without sudo
+usermod -aG docker $USER &&
+sudo setfacl -m user:$USER:rw /var/run/docker.sock &&
+
+# ------------------------------------ Install Docker-Compose ------------------------------------
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+
+chmod +x /usr/local/bin/docker-compose &&
+
+# Install command completion
+curl -L https://raw.githubusercontent.com/docker/compose/1.23.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+# ------------------------------------ Install zsh ----------------------------------------------
 echo 'Install oh-my-zsh'
 echo '-----------------'
 rm -rf $HOME/.oh-my-zsh
